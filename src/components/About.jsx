e64050889016221548c31395afa5928f19739e63
AbnerBenicio <abnerbeniciosilva123@gmail.com>
2023-08-06 14:39:42 -0300

    fix: add _blank no botão de onde assistir

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
             <h1>Rick and Morty</h1>
             <p>Criado por Dan Harmon e Justin Roiland, Rick and Morty acompanha as aventuras do cientista bêbado Rick e seu neto covarde, Morty. A série segue uma fórmula praticamente procedural, em que episódios semanais contam histórias isoladas, com uma ou outra conexão ao longo das temporadas, mas focada em situações semanais através do tempo e espaço.</p>
             <img src={AboutImg}/>
-            <a href="https://www.hbomax.com/br/pt/series/urn:hbo:series:GXkRjxwjR68PDwwEAABKJ">Assista aqui</a>
+            <a href="https://www.hbomax.com/br/pt/series/urn:hbo:series:GXkRjxwjR68PDwwEAABKJ" target="_blank" rel="noreferrer">Assista aqui</a>
         </article>
     )
 }
